ffa90dfa20e143066463a18b8b093ecab6311a91
glaucomorandini <glaucomorandini@hotmail.com>
2023-10-27 22:30:00 -0300

    added transactions csv to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('db', 'transactional-sample.csv'))
+csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+
+csv.each do |row|
+  Transaction.create(
+    transaction_id: row['transaction_id'],
+    merchant_id: row['merchant_id'],
+    user_id: row['user_id'],
+    card_number: row['card_number'],
+    transaction_date: row['transaction_date'],
+    transaction_amount: row['transaction_amount'],
+    device_id: row['device_id'],
+    has_cbk: row['has_cbk']
+  )
+
+  puts "Transaction_id #{row['transaction_id']} saved"
+end
+
+puts "There are now #{Transaction.count} rows in the transactions table"
