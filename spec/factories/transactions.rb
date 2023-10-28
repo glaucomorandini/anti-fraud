83015f3d815a9adfad83e103a1792e5253808f91
glaucomorandini <glaucomorandini@hotmail.com>
2023-10-28 17:48:22 -0300

    added new fields to factory

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -4,9 +4,11 @@ FactoryBot.define do
     merchant_id { Faker::Number.number(digits: 10) }
     user_id { Faker::Number.number(digits: 10) }
     card_number { Faker::Finance.credit_card }
-    transaction_date { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
+    transaction_date { Faker::Time.between(from: DateTime.current - 30.minutes, to: DateTime.current) }
     transaction_amount { Faker::Commerce.price(range: 0..1000.0) }
     device_id { Faker::Number.number(digits: 10) }
     has_cbk { Faker::Boolean.boolean }
+    fraud { Faker::Boolean.boolean }
+    contested_fraud { Faker::Boolean.boolean }
   end
 end
