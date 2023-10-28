a77b609d33d0ee1f55986a8779a906e99e4fcbf4
glaucomorandini <glaucomorandini@hotmail.com>
2023-10-27 23:30:26 -0300

    added presence validations and specs

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Transaction < ApplicationRecord
+  validates :transaction_id, presence: true
+  validates :merchant_id, presence: true
+  validates :user_id, presence: true
+  validates :card_number, presence: true
+  validates :transaction_date, presence: true
+  validates :transaction_amount, presence: true
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,5 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    transaction = build(:transaction)
+    expect(transaction).to be_valid
+  end
+
+  it 'is not valid without a transaction_id' do
+    transaction = build(:transaction, transaction_id: nil)
+    expect(transaction).not_to be_valid
+  end
+
+  it 'is not valid without a merchant_id' do
+    transaction = build(:transaction, merchant_id: nil)
+    expect(transaction).not_to be_valid
+  end
+
+  it 'is not valid without a user_id' do
+    transaction = build(:transaction, user_id: nil)
+    expect(transaction).not_to be_valid
+  end
+
+  it 'is not valid without a card_number' do
+    transaction = build(:transaction, card_number: nil)
+    expect(transaction).not_to be_valid
+  end
+
+  it 'is not valid without a transaction_date' do
+    transaction = build(:transaction, transaction_date: nil)
+    expect(transaction).not_to be_valid
+  end
+
+  it 'is not valid without a transaction_amount' do
+    transaction = build(:transaction, transaction_amount: nil)
+    expect(transaction).not_to be_valid
+  end
 end
